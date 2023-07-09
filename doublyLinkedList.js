9c5a9a312a8ee48bf29e36390227d744fdc70295
Marko Anic <anicmarko9>
2023-07-09 03:13:58 +0200

    feat: push method

--- doublyLinkedList.js
+++ doublyLinkedList.js
@@ -14,4 +14,23 @@ class DoublyLinkedList {
     this.tail = this.head;
     this.length = 1;
   }
+
+  // O(1)
+  push(value) {
+    const newHub = new Hub(value);
+    if (!this.head) {
+      this.head = newHub;
+      this.tail = newHub;
+    } else {
+      this.tail.next = newHub;
+      newHub.prev = this.tail;
+      this.tail = newHub;
+    }
+    this.length++;
+    return this;
+  }
 }
+
+let myDoublyLinkedList = new DoublyLinkedList(1);
+myDoublyLinkedList.push(2);
+console.log(myDoublyLinkedList);

e3c5eac20ccc7a1dda583728a1a8e701fbc7a4c0
Marko Anic <anicmarko9>
2023-07-09 03:04:31 +0200

    feat: create list constructor

--- doublyLinkedList.js
+++ doublyLinkedList.js
@@ -5,3 +5,13 @@ class Hub {
     this.prev = null;
   }
 }
+
+class DoublyLinkedList {
+  // create new Node
+  constructor(value) {
+    const newHub = new Hub(value);
+    this.head = newHub;
+    this.tail = this.head;
+    this.length = 1;
+  }
+}

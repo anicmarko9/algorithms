37c6528d98de4aca2ad43eb7e4f6d73251dad111
Marko Anic <anicmarko9>
2023-07-26 22:45:23 +0200

    feat: add peek() method to the queue

--- data_structures/queue.ts
+++ data_structures/queue.ts
@@ -1,5 +1,5 @@
-import { HubTr } from "./binarySearchTree.js";
-import { Hub } from "./singlyLinkedList.js";
+import { HubTr } from './binarySearchTree.js';
+import { Hub } from './singlyLinkedList.js';
 
 export class Queue {
   first: Hub | null;
@@ -40,5 +40,11 @@ export class Queue {
     }
     this.length--;
     return temp;
+  }
+
+  // Get the first item in the queue without removing it
+  // O(1)
+  peek(): Hub | null {
+    return this.first;
   }
 }

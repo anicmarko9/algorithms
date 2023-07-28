eef3b14c608c4ebd7d052ee2c190706651c95735
Marko Anic <anicmarko9>
2023-07-28 23:25:08 +0200

    feat: add method to clone the graph

--- data_structures/graph.ts
+++ data_structures/graph.ts
@@ -78,6 +78,18 @@ export class Graph {
       : false;
   }
 
+  // O(v+k)
+  clone(): Graph {
+    const newGraph = new Graph();
+    for (const vertex in this.adjacencyList) {
+      newGraph.addVertex(vertex);
+      for (const neighbor of this.adjacencyList[vertex]) {
+        newGraph.addEdge(vertex, neighbor);
+      }
+    }
+    return newGraph;
+  }
+
   // O(1)
   clear(): void {
     this.adjacencyList = {};
